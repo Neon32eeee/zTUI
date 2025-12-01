40fd2dbe27af08ebbdd7535a90bb7229b18631c5
NEon32eeee <pi9116759@gmail.com>
2025-12-01 23:10:02 +0300

    Add madul Color

---
+++ src/Color.zig
@@ -0,0 +1,58 @@
+const std = @import("std");
+
+pub const ColorName = enum {
+    none,
+    red,
+    green,
+    yellow,
+    blue,
+};
+
+const ColorsCode = struct {
+    pub const red: []const u8 = "\x1b[31m";
+    pub const green: []const u8 = "\x1b[32m";
+    pub const yellow: []const u8 = "\x1b[33m";
+    pub const blue: []const u8 = "\x1b[34m";
+};
+
+pub fn colorize(t: std.ArrayList([]const u8), c: ColorName, allocator: std.mem.Allocator) !std.ArrayList([]const u8) {
+    defer {
+        for (t.items) |line| {
+            allocator.free(line);
+        }
+        t.deinit();
+    }
+
+    const color_code = switch (c) {
+        ColorName.none => "\x1b[0m",
+        ColorName.red => ColorsCode.red,
+        ColorName.green => ColorsCode.green,
+        ColorName.yellow => ColorsCode.yellow,
+        ColorName.blue => ColorsCode.blue,
+    };
+
+    var colorize_text = std.ArrayList([]const u8).init(allocator);
+
+    if (t.items.len > 1) {
+        const new_first = try std.mem.concat(allocator, u8, &[_][]const u8{ color_code, t.items[0] });
+        try colorize_text.append(new_first);
+        if (t.items.len == 2) {
+            const new_last = try std.mem.concat(allocator, u8, &[_][]const u8{ t.items[1], "\x1b[0m" });
+            try colorize_text.append(new_last);
+        } else {
+            for (1..t.items.len - 2) |i| {
+                const original_slice = t.items[i];
+                const new_middle = try allocator.alloc(u8, original_slice.len);
+                @memcpy(new_middle, original_slice);
+                try colorize_text.append(new_middle);
+            }
+            const new_last = try std.mem.concat(allocator, u8, &[_][]const u8{ t.items[t.items.len - 1], "\x1b[0m" });
+            try colorize_text.append(new_last);
+        }
+    } else {
+        const new_first = try std.mem.concat(allocator, u8, &[_][]const u8{ color_code, t.items[0], "\x1b[0m" });
+        try colorize_text.append(new_first);
+    }
+
+    return colorize_text;
+}
