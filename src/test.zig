3735f0decddb57e1b92e808a4496ab9c3134aa76
NEon32eeee <pi9116759@gmail.com>
2025-12-01 21:52:02 +0300

    Refactor test code

--- src/test.zig
+++ src/test.zig
@@ -2,10 +2,10 @@ const std = @import("std");
 const ztui = @import("main.zig");
 
 pub fn main() !void {
-    var win = try ztui.tui().init(.{.w = try ztui.getTerminalWidth(), .h = 10}, std.heap.page_allocator);
+    var win = try ztui.tui().init(.{ .w = try ztui.getTerminalWidth(), .h = 10 }, std.heap.page_allocator);
     defer win.deinit();
 
-    win.input_init(.{.promt = "Hello"});
+    win.input_init(.{ .prompt = "Hello" });
 
     win.draw();
 
@@ -13,9 +13,7 @@ pub fn main() !void {
     const answer = try win.hearing(&buff);
 
     if (std.mem.eql(u8, answer, "hi")) {
-        try win.append_row("zTUI test text!");
+        try win.appendRow("zTUI test text!");
         win.draw();
-
     }
-
 }
