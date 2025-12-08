28920555c8dc2e890e0141b59e720db5f822a911
NEon32eeee <pi9116759@gmail.com>
2025-12-08 21:01:37 +0300

    Add func 'applyIndentation' for modul 'Word'

--- src/Word.zig
+++ src/Word.zig
@@ -19,7 +19,7 @@ pub fn wrapText(w: usize, t: []const u8, allocator: std.mem.Allocator) !std.Arra
 
     const writer = line_buff.writer();
 
-    const len_none: usize =  1;   
+    const len_none: usize = 1;
     var last_w: usize = w;
     while (it.next()) |word| {
         const len = wordWidth(word);
@@ -27,14 +27,14 @@ pub fn wrapText(w: usize, t: []const u8, allocator: std.mem.Allocator) !std.Arra
 
         if (final_word_len <= last_w) {
             if (line_buff.items.len > 0) {
-               try writer.writeAll(" ");
+                try writer.writeAll(" ");
             }
 
             try writer.writeAll(word);
 
             last_w -= final_word_len;
         } else {
-            if (line_buff.items.len > 0 ) {
+            if (line_buff.items.len > 0) {
                 const final_line = try line_buff.toOwnedSlice();
                 try corrent_row.append(final_line);
             }
@@ -52,4 +52,19 @@ pub fn wrapText(w: usize, t: []const u8, allocator: std.mem.Allocator) !std.Arra
     }
 
     return corrent_row;
+}
+
+pub fn applyIndentation(allocator: std.mem.Allocator, t: std.ArrayList([]const u8), indentation: usize) !std.ArrayList([]const u8) {
+    if (indentation == 0 or t.items.len == 0) return t;
+
+    const indent = try allocator.alloc(u8, indentation);
+    @memset(indent, ' ');
+
+    const old_first = t.items[0];
+    t.items[0] = try std.mem.concat(allocator, u8, &.{ indent, old_first });
+
+    allocator.free(old_first);
+    allocator.free(indent);
+
+    return t;
 }
