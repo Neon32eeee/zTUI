6526bbd4f42a20c1adb524644148523197fe2e49
NEon32eeee <pi9116759@gmail.com>
2025-12-15 17:11:28 +0300

    Update test

--- src/test.zig
+++ src/test.zig
@@ -2,27 +2,40 @@ const std = @import("std");
 const ztui = @import("main.zig");
 
 pub fn main() !void {
-    var win = try ztui.tui().init(.{ .w = try ztui.getTerminalWidth(), .h = 10 }, std.heap.page_allocator);
+    var win = try ztui.tui().init(.{ .w = try ztui.getTerminalWidth(), .h = try ztui.getTerminalHeigth() }, std.heap.page_allocator);
     defer win.deinit();
 
-    win.inputInit(.{ .prompt = "Hello" });
+    try win.inputInit(.{ .prompt = "Hello" });
 
     win.draw();
 
-    var buff: [32]u8 = undefined;
+    var buff: [1024]u8 = undefined;
     const answer = try win.hearing(&buff);
 
     if (std.mem.eql(u8, answer, "hi")) {
-        try win.appendRow("zTUI test text!", .{ .color = .red });
+        try win.appendRow("zTUI test text", .{ .color = .blue });
+
         win.draw();
 
-        win.clearRow();
+        win.clearRow(.{});
+    		try win.appendProgressBar(0);
+
+    		win.draw();
+
+    		for (0..51) |p| {
+    			try win.setProgressBar(p * 2, 0);
+    			win.draw();
+    			std.time.sleep(std.time.ns_per_s);
+    		}
+
+		win.clearProgressBar(.{});
         win.rename("Echo");
-        win.reprompt("echo");
+        try win.reprompt("echo", .{ .color = .green });
 
         win.draw();
+
         const a2 = try win.hearing(&buff);
-        try win.appendRow(a2, .{});
+        try win.appendRow(a2, .{ .color = .green });
         win.draw();
     }
 }
